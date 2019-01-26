ebb0b46f4a9f45522efa1e931aa4ef4adb6a3198
Marshall Wilson <marshallw@iqmetrix.com>
2019-01-26 10:55:34 -0600

    moved dialogue manager to its own folder modified scene manager to background manager

---
+++ PlanetHome/Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using YamlDotNet;
+
+public class BackgroundManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Background;
+
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Change the background to the picture we want and make sure it's visible.
+    /// </summary>
+    /// <param name="backgroundObject"></param>
+    void SetBackground(GameObject backgroundObject)
+    {
+        Background = backgroundObject;
+        Background.SetActive(true);
+    }
+
+    // Update is called once per frame
+}
